5210e1bd61029b3158b66e5e0990c2ab7a2c4b6e
Derek Mueller <dtmuelle@gmail.com>
2019-08-23 18:10:00 -0700

    fix feature specs by removing jest-puppeteer config and setup script

--- __tests__/features/jest.config.js
+++ __tests__/features/jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   clearMocks: true,
   preset: "jest-puppeteer",
-  setupFilesAfterEnv: ['./setupTestFramework.js'],
   testMatch: [
     "**/__tests__/features/**/*.test.[jt]s?(x)",
   ],
